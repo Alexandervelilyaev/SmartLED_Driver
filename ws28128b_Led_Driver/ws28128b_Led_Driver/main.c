5b95a1692876171d3634289b0a0f47323e4ff24b
Alexander Velilyaev <alexandervelilyaev@gmail.com>
2019-07-20 23:55:50 +0300

    Implemented connection with ESP8266 by UART

--- ws28128b_Led_Driver/ws28128b_Led_Driver/main.c
+++ ws28128b_Led_Driver/ws28128b_Led_Driver/main.c
@@ -2,7 +2,8 @@
  * ws28128b_Led_Driver.c
  *
  * Created: 02.07.2019 23:11:23
- * Author : Alexander Velilyaev
+ * Author: Alexander Velilyaev
+ * Contact: Alexandervelilyaev@gmail.com
  */ 
 
 #include "ws2812b.h"
@@ -14,18 +15,25 @@ uint8_t blue = 255;
 
 int main(void)
 {
-    initLEDs();
+	USART_Init(MYUBRR);
+	initLEDs();
 
-    for (uint8_t i = 0; i < N; ++i)
-    {
-	    setColor(red, green, blue);
-    }
+	while (1)
+	{
+		for (uint8_t i = 0; i < N; ++i)
+		{
+			setColor(red, green, blue);
+		}
 
-    flush();
+		flush();
 
-    while (1)
-    {
-	    
-    }
+		uint8_t data = USART_Receive();
+		if (data == 14)
+		{
+			USART_Transmit(15);
+			red = USART_Receive();
+			green = USART_Receive();
+			blue = USART_Receive();
+		}
+	}
 }
-
